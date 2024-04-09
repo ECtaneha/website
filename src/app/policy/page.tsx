8fb37bb8dd2790557c6f61eaa1945fabd659b024
ECtaneha <taneha.20210918@gmail.com>
2024-04-09 19:01:40 +0900

    policy styling

--- src/app/policy/page.tsx
+++ src/app/policy/page.tsx
@@ -1,11 +1,16 @@
 import Link from 'next/link';
 import globalStyles from '../page.module.scss';
 import styles from './page.module.scss';
+import { BreadCrumb } from '@/components/BreadCrumb/BreadCrumb';
 
 export default function Page() {
   return (
     <div className={globalStyles.contentsContainer}>
       <div className={styles.policyContainer}>
+        <BreadCrumb
+          parentPath='トップ'
+          childPath='会社概要'
+        />
         <h1 className={styles.h1}>個人情報保護方針</h1>
         <h2>個人情報保護方針</h2>
         <p>
@@ -13,7 +18,7 @@ export default function Page() {
         </p>
 
         <br />
-        <h2>個人情報の取り扱いについて</h2>
+        <h2 className={styles.h2}>個人情報の取り扱いについて</h2>
         <h3>（1）個人情報の取得</h3>
         <p className={styles.description}>
           当社は個人情報を適法かつ公正な手段により収集致します。ご利用者様に個人情報の提供をお願いする場合は、事前に収集の目的、利用の内容を開示した上で、当社の正当な事業の範囲内で、その目的の達成に必要な限度において、個人情報を収集致します。
@@ -46,25 +51,25 @@ export default function Page() {
         </p>
         <br />
 
-        <h2>個人情報の保護に関する法令・規範の遵守について</h2>
+        <h2 className={styles.h2}>個人情報の保護に関する法令・規範の遵守について</h2>
         <p>
           当社は、当社が保有する個人情報に関して適用される個人情報保護関連法令及び規範を遵守します。また本方針は、日本国の法律、その他規範により判断致します。本方針は、当社の個人情報の取り扱いに関しての基本的な方針を定めるものであり、当社は本方針に則って、個人情報保護法等の法令・規範に基づく個人情報の保護に努めます。
         </p>
         <br />
 
-        <h2>個人情報の安全管理措置について</h2>
+        <h2 className={styles.h2}>個人情報の安全管理措置について</h2>
         <p>
           当社は、個人情報への不正アクセス、個人情報の紛失、破壊、改ざん、漏えい等から保護し、正確性及び安全性を確保するために管理体制を整備し、適切な安全対策を実施致します。個人情報を取り扱う事務所内への部外者の立ち入りを制限し、当社の個人情報保護に関わる役員・職員等全員に対し教育啓発活動を実施するほか、管理責任者を置き個人情報の適切な管理に努めます。
         </p>
         <br />
 
-        <h2>継続的な改善について</h2>
+        <h2 className={styles.h2}>継続的な改善について</h2>
         <p>
           当社は、個人情報保護への取組みについて、日本国の従うべき法令の変更、取り扱い方法、環境の変化に対応するため、継続的に見直し改善を実施致します。
         </p>
         <br />
 
-        <h2>お問い合わせ</h2>
+        <h2 className={styles.h2}>お問い合わせ</h2>
         <p>個人情報の取り扱いに関するお問い合わせは、下記窓口にて受け付けております。</p>
         <p>【個人情報取扱い窓口】</p>
         <p>≪株式会社タイシン≫</p>
@@ -73,7 +78,7 @@ export default function Page() {
         <p>FAX：088-632-7737</p>
         <br />
 
-        <h2>CookieとWebビーコンの利用</h2>
+        <h2 className={styles.h2}>CookieとWebビーコンの利用</h2>
         <p>
           当サイトでは、ご利用者様のアクセス情報を取得するために「Cookie」や「Webビーコン」といった技術を利用しております。これらにより取得した情報はいずれも個人を特定することはできません。
         </p>
@@ -82,7 +87,7 @@ export default function Page() {
         </p>
         <br />
 
-        <h2>Googleアナリティクスの利用について</h2>
+        <h2 className={styles.h2}>Googleアナリティクスの利用について</h2>
         <p>
           当サイトでは、Googleアナリティクスを利用することがあります。GoogleアナリティクスはCookieを利用して当サイトへのアクセス情報を収集します。アクセス情報の収集及び利用方法については、
           <Link
