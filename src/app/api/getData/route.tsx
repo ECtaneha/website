ffe32aa185d2e6fee7da2c05f38c66c9ce1b4330
ECtaneha <taneha.20210918@gmail.com>
2024-05-17 20:20:55 +0900

    cors setting in getData

--- src/app/api/getData/route.tsx
+++ src/app/api/getData/route.tsx
@@ -1,17 +1,20 @@
 import { PrismaClient, announcement } from '@prisma/client'
 import { NextResponse } from 'next/server'
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
+
+export const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
 
 export async function GET(request: Request) {
   const data: announcement[] = await prisma.announcement.findMany();
 
   return new Response(JSON.stringify(data), {
     status: 200,
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-    },
+    headers: corsHeaders
   });
 }
 
