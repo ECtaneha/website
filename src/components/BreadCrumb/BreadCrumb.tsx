181a1ddc8e3c64fcd1d6ec01430b107aad311d23
ECtaneha <taneha.20210918@gmail.com>
2024-05-21 11:59:22 +0900

    home address change to production address in bread crumb component

--- src/components/BreadCrumb/BreadCrumb.tsx
+++ src/components/BreadCrumb/BreadCrumb.tsx
@@ -3,9 +3,11 @@ import styles from './breadCrumb.module.scss';
 import Link from 'next/link';
 
 export const BreadCrumb = (props: { parentPath: string, childPath: string }) => {
+  const url = process.env.NEXT_PUBLIC_PRODUCTION_URL;
+
   return (
     <p className={styles.breadCrumb}>
-      <Link className={styles.breadCrumbParent} href='/'>{props.parentPath}</Link>
+      <Link className={styles.breadCrumbParent} href={`${url}`}>{props.parentPath}</Link>
       <span className={styles.breadCrumb}>{`　>　${props.childPath}`}</span>
     </p>
   )
