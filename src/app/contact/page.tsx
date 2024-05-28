4f804b33c8569525dd54107a8013c25b44f1a1f1
ECtaneha <taneha.20210918@gmail.com>
2024-05-28 13:48:26 +0900

    mailto

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -7,6 +7,7 @@ import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { BreadCrumb } from '@/components/BreadCrumb/BreadCrumb';
 import { Confirmation } from '@/components/Confirmation/Confirmation';
+import Link from 'next/link';
 
 export default function Page() {
   const [title, setTitle] = useState<string>('');
@@ -18,7 +19,7 @@ export default function Page() {
   const [formData, setFormData] = useState(null);
   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
   const { register, handleSubmit, reset } = useForm();
-
+  const [open, setOpen] = useState<boolean>(false);
 
   const onSubmit = async (data: any) => {
     setFormData(data);
@@ -52,14 +53,161 @@ export default function Page() {
     setIsFormSubmitted(false);
   };
 
+  const generateMailtoLink = (user: string, domain: string) => {
+    return `mailto:${user}@${domain}`
+  }
+  const mailtoLink = generateMailtoLink('admin', 'taishin1977.jp');
+
   return (
-    <div style={{height: "600px", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
-      <h1>メンテナンス中</h1>
-      <br />
-      <>
-        <p>ご迷惑をおかけしております。</p>
-        <p>ただいま改修中です。もうしばらくお待ちください。</p>
-      </>
+    <>
+      {open
+      ?
+      <div style={{height: "600px", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
+        <h1>メンテナンス中</h1>
+        <br />
+        <>
+          <p>ご迷惑をおかけしております。</p>
+          <p>ただいま改修中です。もうしばらくお待ちください。</p>
+          <Link href={mailtoLink}>cl26@taishin1977.jp</Link>
+        </>
+      </div>
+      :
+      <div>
+      {isFormSubmitted
+        ? (
+          <Confirmation
+            formData={formData}
+            onSend={handleSend}
+            onEdit={handleEdit}
+          />
+        )
+        : (
+          <div className={globalStyles.contentsWrapper}>
+            <div className={styles.h1Container}>
+              <h1 className={styles.h1}>お問い合わせ</h1>
+            </div>
+            <div className={styles.contactContainer}>
+              <BreadCrumb
+                parentPath='トップ'
+                childPath='お問い合わせ'
+              />
+              <br />
+              <div className={styles.attention2}>
+                <WarningIcon color='warning' className={styles.icon}/>
+                &nbsp;&nbsp;
+                <span>
+                  個人情報、パスワード、ID、クレジットカードなどの情報は、お問い合わせ内容に入力されないようお願いします。
+                </span>
+              </div>
+
+              <p>*は必須項目です。</p>
+
+              <form
+                className={styles.form}
+                onSubmit={handleSubmit(onSubmit)}
+              >
+                <TextField
+                  label='件名 *'
+                  fullWidth={true}
+                  id='title'
+                  placeholder='件名を入力してください'
+                  {...register('title',{
+                    required: true,
+                    maxLength: 50,
+                  })}
+                  sx={{marginTop: 2, borderRadius: '50%'}}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+
+                <TextField
+                  label='会社名/屋号'
+                  fullWidth={true}
+                  id='company'
+                  placeholder='会社名/屋号を入力してください'
+                  {...register('company',{
+                    maxLength: 50,
+                  })}
+                  sx={{marginTop: 2}}
+                  onChange={(e) => setCompany(e.target.value)}
+                />
+
+                <TextField
+                  label='お名前'
+                  fullWidth={true}
+                  id='userName'
+                  placeholder='お名前を入力してください'
+                  {...register('name',{
+                    required: true,
+                    maxLength: 50,
+                  })}
+                  required
+                  sx={{marginTop: 2}}
+                  onChange={(e) => setName(e.target.value)}
+                />
+
+                <TextField
+                  label='メールアドレス'
+                  id='email'
+                  type='email'
+                  fullWidth={true}
+                  placeholder='メールアドレスを入力してください'
+                  {...register('email', {
+                    required: true,
+                    maxLength: 60,
+                    pattern: {
+                      value:
+                        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                      message: 'メールアドレスの形式が不正です',
+                    },
+                  })}
+                  required
+                  sx={{marginTop: 2}}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+
+                <TextField
+                  label='電話番号'
+                  id='tel'
+                  fullWidth={true}
+                  placeholder='メールアドレスを入力してください'
+                  {...register('tel')}
+                  sx={{marginTop: 2}}
+                  onChange={(e) => setTel(e.target.value)}
+                />
+
+                <TextField
+                  label='お問い合わせ内容 *'
+                  fullWidth={true}
+                  multiline
+                  minRows={10}
+                  maxRows={30}
+                  id='content'
+                  placeholder='お問い合わせ内容を入力してください'
+                  {...register('content', {
+                    required: true,
+                    maxLength: 1000,
+                  })}
+                  size='medium'
+                  sx={{marginTop: 2}}
+                  onChange={(e) => setContent(e.target.value)}
+                />
+
+                <button
+                  type='submit'
+                  className={styles.confirmButton}
+                >
+                  確認
+                </button>
+              </form>
+              <br />
+              <br />
+            </div>
+          </div>
+        )
+      }
     </div>
+    }
+    </>
+
   )
 }
