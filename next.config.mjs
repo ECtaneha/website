f3a3f731fedf5b2490706c38f9868e4b0b8a6244
ECtaneha <taneha.20210918@gmail.com>
2024-07-03 13:02:49 +0900

    popUp position change in handleItems container

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@
 const nextConfig = {
   // vercel に deploy する場合はコメントアウト
   // static export する場合はコメントアウトを外す
-  // output: 'export',
-  // trailingSlash: true,
+  output: 'export',
+  trailingSlash: true,
 };
 
 export default nextConfig;
